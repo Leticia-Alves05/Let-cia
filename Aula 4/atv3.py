994691f9c5b781fb03c3471f630bf7e6d253c62d
Leticia-Alves05 <leticia05def@gmail.com>
2025-02-23 12:27:29 -0300

    Add files via upload
    Aula While

---
+++ Aula 4/atv3.py
@@ -0,0 +1,11 @@
+contador = 0
+soma = 0
+
+while contador < 3 :
+    numero = float(input(" Digite um número: "))
+    soma += numero
+    contador += 1
+
+print(f" A soma dos número é: {soma}")
+
+# Faça um programa que peça 3 numeros utilizando o while e mostre a soma entre eles
