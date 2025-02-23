7031ee8c8eadea999de91d5941c31c6196af19a7
Leticia-Alves05 <leticia05def@gmail.com>
2025-02-23 12:33:11 -0300

    Delete Aula 4 directory

--- Aula 4/atv5.py
+++
@@ -1,5 +0,0 @@
-#Faça um programa que peça varios numeros, e após cada numero pergunte se o usuário deseja continuar (ele deve responder 'S' para Sim e 'N' para Não), 
-# quando essa resposta for 'N' você deve parar a repetição e mostrar a média dos numeros digitados.
-
-soma = 0
-contador = 0
