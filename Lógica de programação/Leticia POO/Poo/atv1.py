646e4d247094c784d222475c19ff6351f01b253c
Leticia <leticia05def@gmail.com>
2026-03-03 21:48:37 -0300

    Renomei as pastas da logica de programacao

--- Lógica de programação/Leticia POO/Poo/atv1.py
+++
@@ -1,9 +0,0 @@
-
-class Cachorro:
-    def __init__(self, nome, altura, raca):
-        self.nome = nome
-        self.altura = altura
-        self.raca = raca
-
-chachorro1 = Cachorro('Megue', 50, 'golden')
-print(chachorro1)
--- Lógica de programação/Leticia POO/Poo/atv2.py
+++
@@ -1,9 +0,0 @@
-
-class Pessoa:
-    def __init__(self, nome, idade, peso, genero):
-        self.nome = nome
-        self.idade = idade
-        self.peso = peso
-        self.genero = genero
-
-pessoa1 = Pessoa('Ana' , 25, 60, 'Feminino')
